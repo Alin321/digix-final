5787b38e4dfb885a26ccf68c79299fd206517c09
Alin321 <yo_alyn3@yahoo.com>
2016-05-25 16:56:05 +0300

    Rest-JSON

--- src/main/java/ro/digix/rest/RegisterRestService.java
+++ src/main/java/ro/digix/rest/RegisterRestService.java
@@ -3,11 +3,17 @@ package ro.digix.rest;
 import java.net.URISyntaxException;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,8 +29,34 @@ public class RegisterRestService {
 	@Autowired
 	private UserService userService;
 	
+	
+	@Path("create2")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public User create2(User user) {
+		return userService.create(user);
+	}
+	
+	@Path("/get/{id}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public User getUserById(@PathParam("id") long id) {
+		User user = userService.getUserById(id);
+		System.out.println(user.getEmail());
+		return user;
+	}
+	
+	@Path("get_all")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<User> getAllUsers() {
+		return userService.getAll();
+	}
+	
 	@Path("/create")
 	@POST
+//	@Consumes(MediaType.APPLICATION_JSON)
 	public Response  create(
 			@FormParam("lastName") String lastName,
 			@FormParam("firstName") String firstName,
@@ -52,7 +84,7 @@ public class RegisterRestService {
 		userService.create(u);
 		java.net.URI location = null;
 		try {
-			location = new java.net.URI("../index.jsp?msg="+u.getFirstName()+"-Added");
+			location = new java.net.URI("../home-page.html?msg="+u.getFirstName()+"-Added");
 		} catch (URISyntaxException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
