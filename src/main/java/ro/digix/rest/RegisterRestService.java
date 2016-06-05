e8e924c4ae7579498600abede9b08bdc08822aa1
Alin321 <yo_alyn3@yahoo.com>
2016-06-05 14:47:23 +0300

    Fixed again

--- src/main/java/ro/digix/rest/RegisterRestService.java
+++ src/main/java/ro/digix/rest/RegisterRestService.java
@@ -1,10 +1,13 @@
 package ro.digix.rest;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
-import java.util.Random;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
@@ -28,8 +31,7 @@ public class RegisterRestService {
 
 	@Autowired
 	private UserService userService;
-	
-	
+
 	@Path("create2")
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
@@ -37,7 +39,7 @@ public class RegisterRestService {
 	public User create2(User user) {
 		return userService.create(user);
 	}
-	
+
 	@Path("/get/{id}")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
@@ -46,32 +48,29 @@ public class RegisterRestService {
 		System.out.println(user.getEmail());
 		return user;
 	}
-	
+
 	@Path("get_all")
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<User> getAllUsers() {
 		return userService.getAll();
 	}
-	
+
 	@Path("/create")
 	@POST
-//	@Consumes(MediaType.APPLICATION_JSON)
-	public Response  create(
-			@FormParam("lastName") String lastName,
-			@FormParam("firstName") String firstName,
-			@FormParam("email") String email,
-			@FormParam("password") String password,
-			@FormParam("birthday") String bAux){
-		
-		String [] data = bAux.split("-");
+	// @Consumes(MediaType.APPLICATION_JSON)
+	public Response create(@FormParam("lastName") String lastName, @FormParam("firstName") String firstName,
+			@FormParam("email") String email, @FormParam("password") String password,
+			@FormParam("birthday") String bAux) {
+
+		String[] data = bAux.split("-");
 		Calendar cal = Calendar.getInstance();
-		cal.set(Calendar.YEAR,Integer.parseInt(data[0]));
-		cal.set(Calendar.MONTH,Integer.parseInt(data[1]));
-		cal.set(Calendar.DAY_OF_MONTH,Integer.parseInt(data[2]));
-		
+		cal.set(Calendar.YEAR, Integer.parseInt(data[0]));
+		cal.set(Calendar.MONTH, Integer.parseInt(data[1]));
+		cal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(data[2]));
+
 		Date date = cal.getTime();
-		
+
 		User u = new User();
 		u.setLastName(lastName);
 		u.setFirstName(firstName);
@@ -80,6 +79,16 @@ public class RegisterRestService {
 		u.setId(userService.getNextId());
 		u.setBirthDate(date);
 		userService.create(u);
+		
+		java.nio.file.Path path = Paths.get("D://workspace/digix_github/digix-final/src/main/webapp/upload" + File.separator + email);
+
+		try {
+			Files.createDirectories(path);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		java.net.URI location = null;
 		try {
 			location = new java.net.URI("../log-in.xhtml");
@@ -87,7 +96,7 @@ public class RegisterRestService {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	    return Response.temporaryRedirect(location).build();
+		return Response.temporaryRedirect(location).build();
 	}
-	
+
 }
