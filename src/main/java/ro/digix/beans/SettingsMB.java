ec817677cdeca0a2741aa30316dd9a9faad7eeae
Alin321 <yo_alyn3@yahoo.com>
2016-06-06 23:20:29 +0300

    Change avatar functionality

--- src/main/java/ro/digix/beans/SettingsMB.java
+++ src/main/java/ro/digix/beans/SettingsMB.java
@@ -4,12 +4,15 @@ import java.io.IOException;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import ro.digix.entities.User;
+import ro.digix.entities.UserFile;
+import ro.digix.services.UserFileService;
 import ro.digix.services.UserService;
 
 @Component("settingsMB")
@@ -21,6 +24,9 @@ public class SettingsMB extends BaseMB {
 
 	@Autowired
 	private UserService userService;
+
+	@Autowired
+	private UserFileService userFileService;
 
 	private String oldPassword;
 	private String newPassword;
@@ -37,6 +43,10 @@ public class SettingsMB extends BaseMB {
 		return newPassword;
 	}
 
+	public void setUserFileService(UserFileService userFileService) {
+		this.userFileService = userFileService;
+	}
+
 	public void setNewPassword(String newPassword) {
 		this.newPassword = newPassword;
 	}
@@ -49,32 +59,41 @@ public class SettingsMB extends BaseMB {
 		this.userService = userService;
 	}
 
-	
-	public void changePassword(){
+	public void changePassword() {
 		User u = userService.getUserById(userMB.getId());
-		
-		if(!u.getPassword().equals(Crypter.encryptSHA256(oldPassword))){
+
+		if (!u.getPassword().equals(Crypter.encryptSHA256(oldPassword))) {
 			addMessageToComponent("oldPassword", "Password is incorrect!");
 			return;
 		}
-		
+
 		u.setPassword(Crypter.encryptSHA256(newPassword));
 		userService.update(u);
 		addMessageToComponent("oldPassword", "Succesfully changed!");
-		
+
 	}
-	
-	public String deleteAccount() throws IOException{
+
+	public String deleteAccount() throws IOException {
 		User u = userService.getUserById(userMB.getId());
 		userService.delete(u);
 		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
-	    ec.invalidateSession();
-	    ec.redirect(ec.getRequestContextPath() + "/index.html");
-	    return null;
-		
+		ec.invalidateSession();
+		ec.redirect(ec.getRequestContextPath() + "/index.html");
+		return null;
+
 	}
-	
-	public void changeAvatar(){
+
+	public void changeAvatar(long id) throws IOException{
+		User u = userService.getUserById(userMB.getId());
+		UserFile uf = userFileService.getUserFileById(id);
 		
+		userMB.setAvatar(uf.getLocation());
+		u.setAvatarLocation(uf.getLocation());
+		
+		userService.update(u);
+		
+		ExternalContext ec = FacesContext.getCurrentInstance().getExternalContext();
+	    ec.redirect(((HttpServletRequest) ec.getRequest()).getRequestURI());
+	    
 	}
 }
